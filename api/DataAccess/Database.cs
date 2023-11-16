192002619f52e9e522caab3bae4263110ed0b375
Caitlin Johnson <crjohnson24@crimson.ua.edu>
2023-11-16 13:27:49 -0600

    Caitlin

--- api/DataAccess/Database.cs
+++ api/DataAccess/Database.cs
@@ -11,13 +11,14 @@ namespace api.DataAccess
 
 
         public Database(){
-            string server =" ";
-            string database=" ";
-            string port =" ";
-            string userName=" ";
-            string password =" ";
+            string server ="acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+            string database="fv960uqrfmsdprn6";
+            string port ="3306";
+            string userName="baog8n9gupkcnra4";
+            string password ="qp8lbpmlxrpertfm";
+            string dateTimeConvert = "True";
 
-            cs=$@"server={server}; user={userName}; database={database}; port={port}; password={password}";
+            cs=$@"server={server}; user={userName}; database={database}; port={port}; password={password}; Convert Zero Datetime = {dateTimeConvert};";
         }
 
     }
